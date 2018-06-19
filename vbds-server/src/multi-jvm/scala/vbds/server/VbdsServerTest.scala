50d25f433597d76db393531edc2a6effd952e14e
abrighton <78px3WT5s>
2018-06-19 22:57:58 +0200

    changed test file size

--- vbds-server/src/multi-jvm/scala/vbds/server/VbdsServerTest.scala
+++ vbds-server/src/multi-jvm/scala/vbds/server/VbdsServerTest.scala
@@ -62,8 +62,8 @@ object VbdsServerTest {
   val testFileName = "vbdsTestFile"
 
   // --- Edit this ---
-//  val testFileSizeBytes = 256*256
-  val testFileSizeBytes = 48*48
+//  val testFileSizeBytes = 256*256*2
+  val testFileSizeBytes = 48*48*2
   val numFilesToPublish = 100000
   val printInterval     = 10000
   // ---
